251e45b5ecebefab527e0e97a451798f8412b062
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-10-09 21:26:17 -0500

    Se agrega función agregar al carrito // Aún falta corregir detalles tales como, cuando se agregan dos productos iguales estos deben sumarse y también debe aparecer el total en el carrito

--- src/JS/cart.js
+++ src/JS/cart.js
@@ -1,19 +1,78 @@
-const btnOpenCart = document.querySelectorAll('[data-accion="abrir-carrito"]'); // Selecciona los botones de abrir el carrito por su atributo 'data-accion'.
+import productsData from "./Data/products";
+// Selecciona los botones de abrir el carrito por su atributo 'data-accion'.
+const btnOpenCart = document.querySelectorAll('[data-accion="abrir-carrito"]');
+// Selecciona los botones de cerrar el carrito por su atributo 'data-accion'.
+const btnCloseCart = document.querySelectorAll('[data-accion="cerrar-carrito"]');
+// Obtiene el elemento del carrito por su ID.
+const windowCart = document.getElementById("carrito");
+// Obtiene el botón 'Add to Cart' del HTML
+const addCart = document.getElementById("agregar-al-carrito");
+// Obtiene el contenedor de todo el producto
+const product = document.getElementById("producto");
+// Generamos una variable la cual tendrá la cantidad de productos que el usuario agregue
+let cartProduct = [];
 
-const btnCloseCart = document.querySelectorAll('[data-accion="cerrar-carrito"]'); // Selecciona los botones de cerrar el carrito por su atributo 'data-accion'.
-
-const windowCart = document.getElementById("carrito"); // Obtiene el elemento del carrito por su ID.
-
-const addCart = document.getElementById("agregar-al-carrito"); // Obtiene el botón 'Add to Cart' del HTML
-
-const product = document.getElementById("producto"); // Obtiene el contenedor de todo el producto
-
-let cartProduct = []; // Generamos una variable la cual tendrá la cantidad de productos que el usuario agregue
-
-// Define una función para mostrar el carrito.
+// Función para renderizar el contenido del carrito y mostrarlo al usuario.
 const renderCart = () => {
+  // Agrega la clase "carrito--active" para mostrar la ventana del carrito.
   windowCart.classList.add("carrito--active");
-  console.log(cartProduct);
+
+  // Eliminamos los productos que hay contenidos en el carrito para que no los duplique si se vuelve a abrir
+  const productsInCart = windowCart.querySelectorAll(".carrito__producto");
+  productsInCart.forEach((product) => product.remove());
+
+  // Itera a través de cada producto en el array "cartProduct".
+  cartProduct.forEach((cartProduct) => {
+    /* Se obtiene el precio del archivo products.js
+     siempre y cuando el id del item coincide con el que está en products.js */
+    productsData.products.forEach((productDataBase) => {
+      if (productDataBase.id === cartProduct.id) {
+        cartProduct.price = productDataBase.price;
+      }
+    });
+
+    // Establece la ruta de la imagen que se quiere mostrar dependiendo de la decisión del usuario
+    let thumbSrc = product.querySelectorAll(".producto__thumb-img")[0].src;
+    if (cartProduct.color === "silver") {
+      thumbSrc = "./img/thumbs/silver/silver.jpg";
+    } else if (cartProduct.color === "blue") {
+      thumbSrc = "./img/thumbs/blue/blue.jpg";
+    } else if (cartProduct.color === "black") {
+      thumbSrc = "./img/thumbs/black/black.jpg";
+    }
+    // Genera una plantilla HTML para representar el producto en el carrito.
+    let templateProduct = `
+      <div class="carrito__producto-info">
+        <img src="${thumbSrc}" alt="" class="carrito__thumb" />
+        <div>
+          <p class="carrito__producto-nombre">
+            <span class="carrito__producto-cantidad">${cartProduct.quantity} x </span>${cartProduct.name}
+          </p>
+          <p class="carrito__producto-propiedades">
+            Shipping Type:<span>${cartProduct.ShippingType}</span> Color:<span>${cartProduct.color}</span>
+          </p>
+        </div>
+      </div>
+      <div class="carrito__producto-contenedor-precio">
+        <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+          <i class="bi bi-x"></i>
+        </button>
+        <p class="carrito__producto-precio">$${cartProduct.price * cartProduct.quantity}</p>
+      </div>
+    `;
+
+    // Crea un nuevo elemento div para el producto en el carrito.
+    let itemCart = document.createElement("div");
+
+    // Agrega la clase "carrito__producto" al elemento div creado.
+    itemCart.classList.add("carrito__producto");
+
+    // Asigna la plantilla HTML como contenido del elemento div.
+    itemCart.innerHTML = templateProduct;
+
+    // Agrega el elemento del producto al cuerpo del carrito.
+    windowCart.querySelector(".carrito__body").appendChild(itemCart);
+  });
 };
 
 // Abrir carrito.
