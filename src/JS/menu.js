1132fd6c79f92c934860c1c6155369389b1a66f2
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-10-16 21:00:43 -0500

    Se agrega menú hamburguesa al header y responsive

---
+++ src/JS/menu.js
@@ -0,0 +1,22 @@
+// Obtén el botón de abrir/cerrar el menú
+const nav = document.querySelector(".header__menu");
+const open = document.querySelector(".open__menu");
+const main = document.querySelector(".main");
+
+// Agrega un controlador de eventos al botón
+open.addEventListener("click", () => {
+  // Verifica si el menú está abierto o cerrado
+  if (nav.classList.contains("visible")) {
+    // Cierra el menú
+    nav.classList.toggle("visible");
+  } else {
+    // Abre el menú
+    nav.classList.add("visible");
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (main.contains(e.target) && e.target !== open) {
+    nav.classList.remove("visible");
+  }
+});
